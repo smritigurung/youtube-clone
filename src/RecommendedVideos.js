0df21cdcda4aea5e4064e6585fd90b0a9843db79
smritigurung <smriti.gurung@sjsu.edu>
2021-01-29 17:24:55 -0800

    second commit

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './RecommendedVideos.css'
+import VideoCard from './VideoCard'
 
 function RecommendedVideos () {
   return (
     <div className='recommendedVideos'>
-      <h2>This is where videos go</h2>
+      <h2>Recommended</h2>
+      <div className='recommendedVideos__videos'>
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+      </div>
     </div>
   )
 }
--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './SidebarRow.css'
 
-function SidebarRow ({ Icon, title }) {
+function SidebarRow ({ selected, Icon, title }) {
   return (
-    <div className='sidebarRow'>
+    /* if selected, then add the "selected" class */
+    <div className={`sidebarRow ${selected && "selected"}`}>
       <Icon className='sidebarRow__icon' />
       <h2 className='sidebarRow__title'>{title}</h2>
     </div>
