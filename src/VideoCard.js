0ea7e2fd338ea3a08789ee713177dd564d16b3d6
smritigurung <smriti.gurung@sjsu.edu>
2021-01-30 01:04:09 -0800

    4th commit

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -5,16 +5,17 @@ import Avatar from '@material-ui/core/Avatar'
 function VideoCard ({ image, title, channel, views, timestamp, channelImage }) {
   return (
     <div className='videoCard'>
-      <img src={image} alt='' />
+      <img className='videoCard__thumbnail' src={image} alt='' />
       <div className='videoCard__info'>
         <Avatar className='videoCard__avatar' alt={channel} src={channelImage} />
-        <div className='video__text'>
+        <div className='videoCard__text'>
           <h4>{title}</h4>
           <p>
             {channel}
           </p>
           <p>
-            {views} •
+            {views} ∙
+            {" "}
             {timestamp}
           </p>
         </div>
