8a02113826827196a6f7a31fe260b9ddab170bde
smritigurung <smriti.gurung@sjsu.edu>
2021-02-06 17:02:45 -0800

    8th commit

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -16,68 +16,105 @@ import beautiful from './images/beautiful.png'
 import blackpink from './images/blackpink.jpg'
 import last from './images/last.png'
 import howYouLikeThat from './images/howYouLikeThat.png'
+import migyo from './images/migyo.jpg'
+import coverBeautiful from './images/coverBeautiful.png'
+import coverMissYou from './images/coverMissYou.png'
 
 function RecommendedVideos () {
   return (
     <div className='recommendedVideos'>
       <h2>Recommended</h2>
       <div className='recommendedVideos__videos'>
-        <VideoCard
-          title='[MV] SEJEONG(세정) (gugudan(구구단)) _ If Only(만에 하나) (The Legend of The Blue Sea(푸른 바다의 전설) OST Part.10)'
-          views='6.1M Views'
-          timestamp='4 years ago'
-          channelImage={theK}
-          channel='1theK'
-          image={ifOnly} />
-        <VideoCard
-          title="Beginner's Guide to React w/ Hooks (2020) Free Course"
-          views='72K Views'
-          timestamp='8 months ago'
-          channelImage={ColtSteele}
-          channel='Colt Steele'
-          image={beginner} />
-        <VideoCard
-          title="You're My Garden - Jung Eun Ji (Strong Woman Do Bong Soon OST) | Park Hyung Sik & Park Bo Young"
-          views='247K Views'
-          timestamp='2 years ago'
-          channelImage={MYlimet}
-          channel='MYlimet'
-          image={garden} />
-        <VideoCard
-          title="[MV] VROMANCE - I'm in love (사랑에 빠진 걸까요 Feat 오브로젝트) (Strong Woman Do Bong Soon OST Part 6)"
-          views='231K Views'
-          timestamp='3 years ago'
-          channelImage={OstKPop}
-          channel='OSTKPOP'
-          image={Vromance} />
-        <VideoCard
-          title="Park Bo Gum's beautiful vocals | Yu Hui Yeol's Sketchbook EP483 [ENG SUBS]"
-          views='851K Views'
-          timestamp='9 months ago'
-          channelImage={viu}
-          channel='Viu Singapore'
-          image={ParkBoGum} />
-        <VideoCard
-          title='[도깨비 OST Part 4] 크러쉬 (Crush) - Beautiful MV (ENG Sub)'
-          views='95M Views'
-          timestamp='4 years ago'
-          channelImage={stone}
-          channel='Stone Music Entertainment'
-          image={beautiful} />
-        <VideoCard
-          title="BLACKPINK - '마지막처럼 (AS IF IT'S YOUR LAST)' M/V"
-          views='951M Views'
-          timestamp='3 years ago'
-          channelImage={blackpink}
-          channel='BLACKPINK'
-          image={last} />
-        <VideoCard
-          title="BLACKPINK - 'How You Like That' M/V"
-          views='754M Views'
-          timestamp='7 months ago'
-          channelImage={blackpink}
-          channel='BLACKPINK'
-          image={howYouLikeThat} />
+        <a href='https://www.youtube.com/watch?v=EmjhISspkyY'>
+          <VideoCard
+            title='[MV] SEJEONG(세정) (gugudan(구구단)) _ If Only(만에 하나) (The Legend of The Blue Sea(푸른 바다의 전설) OST Part.10)'
+            views='6.1M Views'
+            timestamp='4 years ago'
+            channelImage={theK}
+            channel='1theK'
+            image={ifOnly} />
+        </a>
+        <a href='https://www.youtube.com/watch?v=9U3IhLAnSxM'>
+          <VideoCard
+            title="Beginner's Guide to React w/ Hooks (2020) Free Course"
+            views='72K Views'
+            timestamp='8 months ago'
+            channelImage={ColtSteele}
+            channel='Colt Steele'
+            image={beginner} />
+        </a>
+        <a href='https://www.youtube.com/watch?v=1VJFdg7wTuo'>
+          <VideoCard
+            title="You're My Garden - Jung Eun Ji (Strong Woman Do Bong Soon OST) | Park Hyung Sik & Park Bo Young"
+            views='247K Views'
+            timestamp='2 years ago'
+            channelImage={MYlimet}
+            channel='MYlimet'
+            image={garden} />
+        </a>
+        <a href='https://www.youtube.com/watch?v=u_saJs6GLqA'>
+          <VideoCard
+            title="[MV] VROMANCE - I'm in love (사랑에 빠진 걸까요 Feat 오브로젝트) (Strong Woman Do Bong Soon OST Part 6)"
+            views='231K Views'
+            timestamp='3 years ago'
+            channelImage={OstKPop}
+            channel='OSTKPOP'
+            image={Vromance} />
+        </a>
+        <a href='https://www.youtube.com/watch?v=68kerCd5n1A'>
+          <VideoCard
+            title="Park Bo Gum's beautiful vocals | Yu Hui Yeol's Sketchbook EP483 [ENG SUBS]"
+            views='851K Views'
+            timestamp='9 months ago'
+            channelImage={viu}
+            channel='Viu Singapore'
+            image={ParkBoGum} />
+        </a>
+        <a href='https://www.youtube.com/watch?v=W0cs6ciCt_k'>
+          <VideoCard
+            title='[도깨비 OST Part 4] 크러쉬 (Crush) - Beautiful MV (ENG Sub)'
+            views='95M Views'
+            timestamp='4 years ago'
+            channelImage={stone}
+            channel='Stone Music Entertainment'
+            image={beautiful} />
+        </a>
+        <a href='https://www.youtube.com/watch?v=Amq-qlqbjYA'>
+          <VideoCard
+            title="BLACKPINK - '마지막처럼 (AS IF IT'S YOUR LAST)' M/V"
+            views='951M Views'
+            timestamp='3 years ago'
+            channelImage={blackpink}
+            channel='BLACKPINK'
+            image={last} />
+        </a>
+        <a href='https://www.youtube.com/watch?v=ioNng23DkIM'>
+          <VideoCard
+            title="BLACKPINK - 'How You Like That' M/V"
+            views='754M Views'
+            timestamp='7 months ago'
+            channelImage={blackpink}
+            channel='BLACKPINK'
+            image={howYouLikeThat} />
+        </a>
+        <a href='https://www.youtube.com/watch?v=azgicrmD-J0'>
+          <VideoCard
+            title="Beautiful - 크러쉬 (드라마 '도깨비' OST) / Cover by 미교(Migyo)"
+            views='333K Views'
+            timestamp='4 years ago'
+            channelImage={migyo}
+            channel='쩐MIGYO'
+            image={coverBeautiful} />
+        </a>
+        <a href='https://www.youtube.com/watch?v=E3ZNwQXDaeY'>
+          <VideoCard
+            title="I miss you - 소유 (드라마 '도깨비' OST) / Cover by 미교(Migyo)"
+            views='419K Views'
+            timestamp='3 years ago'
+            channelImage={migyo}
+            channel='쩐MIGYO'
+            image={coverMissYou} />
+        </a>
       </div>
     </div>
   )
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -4,6 +4,11 @@ import VideoRow from './VideoRow'
 import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined'
 import blackpink from './images/blackpink.jpg'
 import howYouLikeThat from './images/howYouLikeThat.png'
+import solo from './images/solo.png'
+import BPFire from './images/BPFire.png'
+import BP2020 from './images/BP2020.png'
+import BPddu from './images/BPddu.png'
+import BPwhistle from './images/BPwhistle.png'
 
 import './SearchPage.css'
 
@@ -23,14 +28,66 @@ function SearchPage () {
         numOfVideos={336}
         description='BLACKPINK Official YouTube Channel 블랙핑크 공식 유튜브 채널입니다. JISOO, JENNIE, ROSÉ, LISA 지수, 제니, 로제, 리사' />
       <hr />
-      <VideoRow
-        views='754M'
-        subs='56.7M'
-        description="BLACKPINK - 'How You Like That'"
-        timestamp='7 months ago'
-        channel='BLACKPINK'
-        title="BLACKPINK - 'How You Like That' M/V"
-        image={howYouLikeThat} />
+      <a href='https://www.youtube.com/watch?v=ioNng23DkIM'>
+        <VideoRow
+          views='754M'
+          subs='56.8M'
+          description="BLACKPINK - 'How You Like That'"
+          timestamp='7 months ago'
+          channel='BLACKPINK'
+          title="BLACKPINK - 'How You Like That' M/V"
+          image={howYouLikeThat} />
+      </a>
+      <a href='https://www.youtube.com/watch?v=b73BI9eUkjM'>
+        <VideoRow
+          views='611M'
+          subs='56.8M'
+          description='Download on Apple Music @ http://smarturl.it/SOLOJennie/applemusic'
+          timestamp='2 years ago'
+          channel='BLACKPINK'
+          title="JENNIE - 'SOLO' M/V"
+          image={solo} />
+      </a>
+      <a href='https://www.youtube.com/watch?v=9pdj4iJD08s'>
+        <VideoRow
+          views='613M'
+          subs='56.8M'
+          description='Available @ https://BLACKPINK.lnk.to/SQUARETWOID'
+          timestamp='4 years ago'
+          channel='BLACKPINK'
+          title="BLACKPINK - '불장난 (PLAYING WITH FIRE)' M/V"
+          image={BPFire} />
+      </a>
+      <a href='https://www.youtube.com/watch?v=JvCBLBOXBuI'>
+        <VideoRow
+          views='613M'
+          subs='3.8M'
+          description='#BLACKPINK​ #블랙핑크​ #JISOO​ #지수​ #JENNIE​ #제니​ #ROSÉ​ #로제​ #LISA​ #리사​'
+          timestamp='11 months ago'
+          channel='BLACKPINK'
+          title="BLACKPINK - BLACKPINK'S 2020 WELCOMING COLLECTION PREVIEW"
+          image={BP2020} />
+      </a>
+      <a href='https://www.youtube.com/watch?v=IHNzOHi8sJs'>
+        <VideoRow
+          views='1.4B'
+          subs='3.8M'
+          description='Download on iTunes @ http://smarturl.it/BP_SquareUp/itunes​'
+          timestamp='2 years ago'
+          channel='BLACKPINK'
+          title='BLACKPINK - ‘뚜두뚜두 (DDU-DU DDU-DU)’ M/V'
+          image={BPddu} />
+      </a>
+      <a href='https://www.youtube.com/watch?v=dISNgvVpWlo'>
+        <VideoRow
+          views='615M'
+          subs='3.8M'
+          description='Available @ https://BLACKPINK.lnk.to/SQUAREONEID​'
+          timestamp='4 years ago'
+          channel='BLACKPINK'
+          title="BLACKPINK - '휘파람'(WHISTLE) M/V"
+          image={BPwhistle} />
+      </a>
     </div>
   )
 }
